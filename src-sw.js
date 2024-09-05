e2dec5898cc34f6dba957acc25024c782d067c9d
bahlool1401 <bahlool.ramesh@gmail.com>
2024-09-06 02:29:54 +0330

    add web manifest

--- src-sw.js
+++ src-sw.js
@@ -5,6 +5,14 @@ workbox.routing.registerRoute(
     /https:\/\/toplearn\.com\/flights\.json/,
     // OR /https:\/\/toplearn\.com\/*\.json/,
     new workbox.strategies.NetworkFirst({
-       cacheName:"api-cache" 
+       cacheName:"api-cache",
+       plugins:[
+        new workbox.expiration.Plugin({
+            maxEntries:50,
+            purgeOnQuotaError:true,
+            //if cache is loaded, the previous cache will be deleted👆
+            maxAgeSeconds:10 * 86400 //10 days
+        })
+       ]
     })
 )
